91acc12fc1b4113e422df9c914d68f951fbbf95f
Compiler <lukerochedev@gmail.com>
2020-07-16 11:47:48 -0400

    .

--- CMD Build/build-misc/build.bat
+++ CMD Build/build-misc/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\build
 pushd ..\build
-cl -Zi ..\src\win32_lhmh.cpp
+cl -Zi ..\src\win32_lhmh.cpp user32.lib
 popd
--- CMD Build/src/win32_lhmh.cpp
+++ CMD Build/src/win32_lhmh.cpp
@@ -1,6 +1,8 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd) {
 
-    return 0;
+//C runtime lib entry point
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd) {
+    MessageBox(0, "Sup dilligent", "Dilligence", MB_OK | MB_ICONINFORMATION);
+    return(0);
 }
