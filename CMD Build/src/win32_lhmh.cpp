49ea6b1713e248601003f1349585a2a922446330
Compiler <lukerochedev@gmail.com>
2020-07-17 07:03:27 -0400

    winproc startup handling

--- CMD Build/src/win32_lhmh.cpp
+++ CMD Build/src/win32_lhmh.cpp
@@ -1,8 +1,54 @@
 #include <windows.h>
 
+//handle to window, message, 
+LRESULT CALLBACK mainWindowCallback( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+
+    LRESULT result = 0;
+    switch(uMsg){
+
+        //window size changed
+        case WM_SIZE:{
+            OutputDebugStringA("WM_SIZE\n");
+        }break;
+
+        //window deletes window
+        case WM_DESTROY:{
+            OutputDebugStringA("WM_DESTROY\n");
+                
+        }break;
+
+        //window instructed to close
+        case WM_CLOSE:{
+            OutputDebugStringA("WM_CLOSE\n");
+                
+        }break;
+
+        //user has clicked to focus the window
+        case WM_ACTIVATEAPP:{
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+                
+        }break;
+        
+        default:{
+           // OutputDebugStringA("Default\n");
+
+            result = DefWindowProc(hwnd, uMsg, wParam, lParam);//defualt window callback handler for actions we dont handle
+        }break;
+
+    }
+    return result;
+
+}
 
 //C runtime lib entry point
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd) {
-    MessageBox(0, "Sup dilligent", "Dilligence", MB_OK | MB_ICONINFORMATION);
+    OutputDebugStringA("App start\n");
+    WNDCLASS windowClass = {};
+    windowClass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW; //allocates a unique device context for each window in the class
+    windowClass.lpfnWndProc =  mainWindowCallback;
+    windowClass.hInstance = hInstance;
+    //windowClass.hIcon = ;
+    windowClass.lpszClassName = "LHMHWindowClass";
     return(0);
 }
+
